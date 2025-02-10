fe1e7dbac1815cd1a0ffef7d054d794f436ad906
Mayank Gaur <85687789+coffeeandtravel@users.noreply.github.com>
2025-02-10 19:51:40 +0530

    removed useless lines of code

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,3 @@
-// import './App.css'
-// import Content from './assets/components/Content'
-// import Hover from './assets/components/Hover'
-// import Sidebar from './assets/components/SIdebar'
-
-// function App() {
-//   return (
-//     <>
-//     <Hover/>
-//     <div className='h-screen justify-center flex sticky top-0 scroll-smooth'>
-//       <div className="flex justify-center items-center w-full sticky top-0">
-//   <div className="flex md:flex-row flex-col justify-items-start lg:flex-row md:justify-center lg:justify-center lg:gap-10 items-stretch w-full lg:max-w-6xl">
-//           <Sidebar/>
-//           <Content/>
-//         </div>
-//       </div>
-//     </div>
-//     </>
-//   )
-// }
-
-// export default App
-
 import './App.css'
 import Content from './assets/components/Content'
 import Hover from './assets/components/Hover'
--- src/assets/components/SideNav.jsx
+++ src/assets/components/SideNav.jsx
@@ -1,5 +1,3 @@
-// import React from 'react'
-
 const SideNav = () => {
   return (
     <div>
